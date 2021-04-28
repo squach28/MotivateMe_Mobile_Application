ecf43d858dc8d1973831bf767aa925fee1f70f93
yaoyu14 <yao97ming@gmail.com>
2021-04-27 19:19:07 -0700

    Calendar update

--- lib/utils.dart
+++ lib/utils.dart
@@ -11,45 +11,19 @@ import 'model/subgoal.dart';
 class CalendarUtils {
   final goalManager = GoalManager();
 
-  var kEvents = LinkedHashMap<DateTime, List<SubGoal>>(
-    equals: isSameDay,
-    hashCode: getHashCode,
-  ); 
+  Future<Map<DateTime, List<dynamic>>> fetchSubGoals() async {
+    List<dynamic> subGoals = await goalManager.retrieveSubGoals();
 
-  Future<Map<DateTime,List<SubGoal>>> fetchSubGoals() async {
-    List<SubGoal> subGoals = await goalManager.retrieveSubGoals();
-    
-    Map<DateTime, List<SubGoal>> _kEventSource = {};
+    Map<DateTime, List<dynamic>> _kEventSource = {};
 
     for (SubGoal subGoal in subGoals) {
-      if(!_kEventSource.keys.contains(subGoal.date)) {
+      if (!_kEventSource.keys.contains(subGoal.date)) {
         _kEventSource[subGoal.date] = [subGoal];
       } else {
         _kEventSource[subGoal.date].add(subGoal);
       }
-    } 
-
-
-    kEvents.addAll(_kEventSource);
+    }
     return _kEventSource;
-  }
-  // TODO change _kEventSOurce in a function and then have calendar.dart use a future builder based on that function
-
-  final kFirstDay = DateTime(
-      DateTime.now().year - 2, DateTime.now().month, DateTime.now().day);
-  final kLastDay = DateTime(
-      DateTime.now().year + 2, DateTime.now().month, DateTime.now().day);
-  static int getHashCode(DateTime key) {
-    return key.day * 1000000 + key.month * 10000 + key.year;
-  }
-
-  /// Returns a list of [DateTime] objects from [first] to [last], inclusive.
-  List<DateTime> daysInRange(DateTime first, DateTime last) {
-    final dayCount = last.difference(first).inDays + 1;
-    return List.generate(
-      dayCount,
-      (index) => DateTime.utc(first.year, first.month, first.day + index),
-    );
   }
 }
 
